a1c74ec474b101b326460475f7197ae23f2c1b6a
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2023-12-29 16:12:20 +0100

    result percentage displayer simplified

--- src/components/resultGauge.js
+++ src/components/resultGauge.js
@@ -1,12 +1,7 @@
 import React from "react";
 
-function ResultGauge({ result, theme, ranking }) {
+function ResultGauge({ result}) {
   const percentage = result.probability * 100;
-  let gaugeWidth = 100;
-  let gaugeHeight = 8;
-
-  const percentageWidth =
-    percentage ** 1.75 * (100 / 100 ** 1.75) * (gaugeWidth / 100) + "%";
 
   const colors = [
     [170, 0, 0],
@@ -15,6 +10,7 @@ function ResultGauge({ result, theme, ranking }) {
   ];
 
   const getColor = (percent) => {
+    // TODO: choose some specific colors instead of a whole range.
     const colorIndex = (percent / 100.0) * (colors.length - 1);
     const lowFactor = 1 - (colorIndex % 1);
     const lowIndex = Math.floor(colorIndex);
@@ -31,13 +27,9 @@ function ResultGauge({ result, theme, ranking }) {
   };
 
   let color = getColor(percentage);
-  let ticks = [];
-
-  for (let i = 10; i < 100; i = i + 10) {
-    ticks = ticks.concat([i]);
-  }
 
   const getText = (percentage) =>{
+    // TODO: Adjust text and also steps
     if(percentage > 90){
       return "svært høy treffprosent"
     }else if(percentage > 70){
@@ -65,56 +57,11 @@ function ResultGauge({ result, theme, ranking }) {
           display:"inline-block",
           marginRight:"4px"
         }}
-      />
-      
-     { getText(percentage)}
-
-      
+      />      
+     { getText(percentage)}      
     </>
   )
 
-  return (
-    <>
-      <div
-        style={{
-          position: "relative",
-          marginTop: gaugeHeight * 2,
-          width: gaugeWidth + "%",
-          height: gaugeHeight,
-          backgroundColor: color,
-          opacity: 0.15,
-          borderRadius: gaugeHeight / 2,
-        }}
-      />
-
-      <div
-        style={{
-          position: "relative",
-          width: percentageWidth,
-          paddingLeft: "2%",
-          height: gaugeHeight,
-          backgroundColor: color,
-          borderRadius: gaugeHeight / 2,
-          marginTop: -gaugeHeight,
-        }}
-      />
-
-      {ticks.map((t) => (
-        <div
-          key={t}
-          style={{
-            position: "relative",
-            width: 2,
-            height: gaugeHeight,
-            marginTop: -gaugeHeight,
-            marginLeft:
-              t ** 1.75 * (100 / 100 ** 1.75) * (gaugeWidth / 100) + "%",
-          }}
-          className="tick"
-        />
-      ))}
-    </>
-  );
 }
 
 export default ResultGauge;
